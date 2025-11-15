f81e9b6ffdd2f7b4ef2a1804fbac5dc10c5eb42a
brandon-renner <brenner3@kent.edu>
2025-11-15 13:09:01 -0500

    Apriori Frequent Itemsets Base Algorithm

--- BaseRecommendation.cpp
+++ BaseRecommendation.cpp
@@ -1,1 +1,148 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <set>
+#include <vector>
 
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+vector<set<string>> readTransactions(const string &filename)
+{
+    ifstream infile(filename);
+    vector<set<string>> transactions;
+    string line;
+
+    while (getline(infile, line))
+    {
+        set<string> transaction;
+        size_t start = 0, end;
+
+        while ((end = line.find(' ', start)) != string::npos)
+        {
+            string item = line.substr(start, end - start);
+            if (!item.empty())
+                transaction.insert(item);
+            start = end + 1;
+        }
+
+        string lastItem = line.substr(start);
+        if (!lastItem.empty())
+            transaction.insert(lastItem);
+
+        if (!transaction.empty())
+            transactions.push_back(transaction);
+    }
+
+    return transactions;
+}
+
+vector<set<string>> generateC1(const vector<set<string>> &transactions)
+{
+    set<string> all_items;
+    for (auto &t : transactions)
+        all_items.insert(t.begin(), t.end());
+
+    vector<set<string>> C1;
+    for (auto &item : all_items)
+        C1.push_back({item});
+    return C1;
+}
+
+int countSupport(const set<string> &candidate, const vector<set<string>> &transactions)
+{
+    int count = 0;
+    for (auto &t : transactions)
+        if (includes(t.begin(), t.end(), candidate.begin(), candidate.end()))
+            count++;
+    return count;
+}
+
+vector<set<string>> filterCandidates(const vector<set<string>> &Ck,
+                                     const vector<set<string>> &transactions,
+                                     int min_support_count,
+                                     map<set<string>, int> &support_map)
+{
+    vector<set<string>> Lk;
+    for (auto &cand : Ck)
+    {
+        int count = countSupport(cand, transactions);
+        support_map[cand] = count;
+        if (count >= min_support_count)
+            Lk.push_back(cand);
+    }
+    return Lk;
+}
+
+vector<set<string>> aprioriGen(const vector<set<string>> &Lprev)
+{
+    vector<set<string>> Ck;
+    int k = Lprev[0].size() + 1;
+
+    for (size_t i = 0; i < Lprev.size(); i++)
+    {
+        for (size_t j = i + 1; j < Lprev.size(); j++)
+        {
+            vector<string> v1(Lprev[i].begin(), Lprev[i].end());
+            vector<string> v2(Lprev[j].begin(), Lprev[j].end());
+
+            bool joinable = true;
+            for (int x = 0; x < k - 2; x++)
+            {
+                if (v1[x] != v2[x])
+                {
+                    joinable = false;
+                    break;
+                }
+            }
+
+            if (joinable)
+            {
+                set<string> candidate = Lprev[i];
+                candidate.insert(v2.back());
+                Ck.push_back(candidate);
+            }
+        }
+    }
+    return Ck;
+}
+
+int main()
+{
+
+    string filename = "input.txt";
+    double min_support_percent = 0;
+
+    vector<set<string>> transactions = readTransactions(filename);
+    int num_transactions = transactions.size();
+    int min_support_count = ceil((min_support_percent / 100.0) * num_transactions);
+
+    map<set<string>, int> support_map;
+    vector<set<string>> C1 = generateC1(transactions);
+    vector<set<string>> Lk = filterCandidates(C1, transactions, min_support_count, support_map);
+
+    vector<set<string>> all_frequent;
+    all_frequent.insert(all_frequent.end(), Lk.begin(), Lk.end());
+
+    while (!Lk.empty())
+    {
+        vector<set<string>> Ck = aprioriGen(Lk);
+        Lk = filterCandidates(Ck, transactions, min_support_count, support_map);
+        all_frequent.insert(all_frequent.end(), Lk.begin(), Lk.end());
+    }
+
+    cout << "Frequent Itemsets:\n";
+    for (auto &itemset : all_frequent)
+    {
+        double support = (support_map[itemset] * 100.0) / num_transactions;
+        cout << "{ ";
+        for (auto &item : itemset)
+            cout << item << " ";
+        cout << "} -> Support: " << fixed << setprecision(0) << support / 10 << " or " << support << "%\n";
+    }
+
+    return 0;
+}
