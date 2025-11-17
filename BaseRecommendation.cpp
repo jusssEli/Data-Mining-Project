41247964c7c2be8d63b02d2df0fb65eb5860dbff
brandon-renner <brenner3@kent.edu>
2025-11-17 13:13:30 -0500

    New Base Recommender

--- BaseRecommendation.cpp
+++ BaseRecommendation.cpp
@@ -18,10 +18,10 @@ struct Rule
 
     bool operator<(Rule const &other) const
     {
-        if (precedent_ < other.precedent_)
-            return true;
-        if (other.precedent_ < precedent_)
-            return false;
+        if (precedent_ != other.precedent_)
+        {
+            return precedent_ < other.precedent_;
+        }
         return antecendent_ < other.antecendent_;
     }
 };
@@ -73,24 +73,30 @@ vector<set<string>> generateC1(const vector<set<string>> &transactions)
     return C1;
 }
 
-int countSupport(const set<string> &candidate, const vector<set<string>> &transactions)
+double countSupport(const set<string> &candidate, const vector<set<string>> &transactions)
 {
-    int count = 0;
+    double count = 0;
     for (auto &t : transactions)
-        if (includes(t.begin(), t.end(), candidate.begin(), candidate.end()))
+    {
+        bool foundFlag = true;
+        for (auto &c : candidate)
+            if (t.find(c) == t.end())
+                foundFlag = false;
+        if (foundFlag)
             count++;
+    }
     return count;
 }
 
 vector<set<string>> filterCandidates(const vector<set<string>> &Ck,
                                      const vector<set<string>> &transactions,
-                                     int min_support_count,
-                                     map<set<string>, int> &support_map)
+                                     double min_support_count,
+                                     map<set<string>, double> &support_map)
 {
     vector<set<string>> Lk;
     for (auto &cand : Ck)
     {
-        int count = countSupport(cand, transactions);
+        double count = countSupport(cand, transactions);
         support_map[cand] = count;
         if (count >= min_support_count)
             Lk.push_back(cand);
@@ -134,14 +140,17 @@ vector<set<string>> aprioriGen(const vector<set<string>> &Lprev)
 int main()
 {
 
+    // Apriori Algorithm Logic, produces itemsets with supports ------------------------------------------
+
     string filename = "transactions.txt";
-    double min_support_percent = 0;
+    double min_support_percent = 0;    // set to zero because we want all the items/sets
+    double min_rule_confidence = 0.50; // 50%
 
     vector<set<string>> transactions = readTransactions(filename);
     int num_transactions = transactions.size();
     int min_support_count = ceil((min_support_percent / 100.0) * num_transactions);
 
-    map<set<string>, int> support_map;
+    map<set<string>, double> support_map;
     vector<set<string>> C1 = generateC1(transactions);
     vector<set<string>> Lk = filterCandidates(C1, transactions, min_support_count, support_map);
 
@@ -155,7 +164,11 @@ int main()
         all_frequent.insert(all_frequent.end(), Lk.begin(), Lk.end());
     }
 
-    multiset<Rule> Rules;
+    //------------------------------------------------------------------------------------------------------
+
+    // Generate all possible Rules from collection of itemsets
+    // takes forever
+    set<Rule> Rules;
     int maxCount = all_frequent[all_frequent.size() - 1].size();
     for (auto &itemset_prec : all_frequent)
     {
@@ -184,8 +197,51 @@ int main()
         }
     }
 
-    cout << "Recommender Rules:\n";
+    // Set to hold "Valid" Rules, defined below
+    set<Rule> ValidRules;
+
+    // Select "Valid" Rules from set of all possible Rules
+    // takes forever
     for (auto &rule : Rules)
+    {
+        set<string> ruleUnion; // union for confidence caluclation
+        set_union(rule.precedent_.begin(), rule.precedent_.end(),
+                  rule.antecendent_.begin(), rule.antecendent_.end(), inserter(ruleUnion, ruleUnion.begin()));
+        // if the precendent/Union is actually in the support map, continue
+        if (support_map.find(ruleUnion) != support_map.end() &&
+            support_map.find(rule.precedent_) != support_map.end())
+        {
+            // if its supports are greater than 3 (arbitrart level to weed out small samples)
+            if (support_map[rule.precedent_] > 3 && support_map[ruleUnion] > 3)
+            {
+                Rule tempRule;
+                tempRule.precedent_ = rule.precedent_;
+                tempRule.antecendent_ = rule.antecendent_;
+                double numerator = support_map[ruleUnion];
+                double denominator = support_map[rule.precedent_];
+                tempRule.confidence_ = numerator / denominator;
+                ValidRules.insert(tempRule);
+            }
+        }
+    }
+
+    // Narrow down valid rules further:
+    // only keep ones with high enough confidence && not 1:1 rules (remove redundant, not good rules)
+    for (auto it = ValidRules.begin(); it != ValidRules.end();)
+    {
+        if (it->confidence_ < min_rule_confidence || (it->precedent_.size() == 1 && it->antecendent_.size() == 1))
+        {
+            it = ValidRules.erase(it);
+        }
+        else
+        {
+            ++it;
+        }
+    }
+
+    // Print out Rules
+    cout << "Recommender Rules:\n";
+    for (auto &rule : ValidRules)
     {
         cout << "{ ";
         for (auto &item : rule.precedent_)
@@ -193,7 +249,7 @@ int main()
         cout << "} -> { ";
         for (auto &item : rule.antecendent_)
             cout << item << " ";
-        cout << "}\n";
+        cout << "} : Confidence: " << rule.confidence_ * 100 << "\n";
     }
 
     return 0;
